77c1967d9995d71ac5c6ecd46ce34449938f8ba2
Josh Mansito <joshua.mansito@ucdenver.edu>
2021-05-01 23:58:42 -0600

    Formatting update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '55bfcd2a220e246f1164249d70a1b53667884d7b',
+    'name' => 'vendor_name/rentalwebsite',
+  ),
+  'versions' => 
+  array (
+    'monolog/monolog' => 
+    array (
+      'pretty_version' => '1.0.2',
+      'version' => '1.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b704c49a3051536f67f2d39f13568f74615b9922',
+    ),
+    'vendor_name/rentalwebsite' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '55bfcd2a220e246f1164249d70a1b53667884d7b',
+    ),
+  ),
+);
