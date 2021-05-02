7bc958a44591ac3bd08fc90816f0e6ad45bc9fad
Josh Mansito <joshua.mansito@ucdenver.edu>
2021-05-02 02:37:04 -0600

    Display Tables

--- RentalWebsite/showDatabase.php
+++ RentalWebsite/showDatabase.php
@@ -28,7 +28,7 @@
         $database = "HW2";
         $mysqli = new mysqli("localhost", $username, $password, $database);
 
-        //Customer database
+        //Customer table
         $query = "SELECT * FROM customer";
 
         echo'<h3>Customers</h3>';
@@ -55,7 +55,7 @@
             $result->free();
         }
 
-        //car database
+        //car table
         $query = "SELECT * FROM car";
 
         echo '<table border="2" cellspacing="2" cellpadding="2" bgcolor="#dcdcdc"> 
@@ -86,6 +86,88 @@
             $result->free();
         }
 
+        //CarType table
+        $query = "SELECT * FROM cartype";
+
+        echo '<table border="2" cellspacing="2" cellpadding="2" bgcolor="#dcdcdc"> 
+      <tr> 
+          <td> <b><font face="Arial">CarType</font></b> </td> 
+          <td> <b><font face="Arial">EffectiveDate</font></b> </td> 
+          <td> <b><font face="Arial">DailyRate</font></b> </td> 
+          <td> <b><font face="Arial">WeeklyRate</font></b> </td> 
+      </tr>';
+        if ($result = $mysqli->query($query)) {
+            echo'<h3>Car Types</h3>';
+            while ($row = $result->fetch_assoc()) {
+                $CTField1 = $row["CarType"];
+                $CTField2 = $row["EffectiveDate"];
+                $CTField3 = $row["DailyRate"];
+                $CTField4 = $row["WeeklyRate"];
+
+                echo '<tr> 
+                  <td>'.$CTField1.'</td> 
+                  <td>'.$CTField2.'</td> 
+                  <td>'.$CTField3.'</td> 
+                  <td>'.$CTField4.'</td> 
+              </tr>';
+            }
+            $result->free();
+        }
+
+        //Rental table
+        $query = "SELECT * FROM rental";
+
+        echo '<table border="2" cellspacing="2" cellpadding="2" bgcolor="#dcdcdc"> 
+      <tr> 
+          <td> <b><font face="Arial">RentalID</font></b> </td> 
+          <td> <b><font face="Arial">IdNo</font></b> </td> 
+          <td> <b><font face="Arial">VehicleID</font></b> </td> 
+          <td> <b><font face="Arial">Term</font></b> </td> 
+          <td> <b><font face="Arial">StartDate</font></b> </td>
+          <td> <b><font face="Arial">DailyTerm</font></b> </td>
+          <td> <b><font face="Arial">WeeklyTerm</font></b> </td>
+          <td> <b><font face="Arial">NoOfDays</font></b> </td>
+          <td> <b><font face="Arial">NoOfWeeks</font></b> </td>
+          <td> <b><font face="Arial">ActualReturnDate</font></b> </td>
+          <td> <b><font face="Arial">AmountDue</font></b> </td>
+          <td> <b><font face="Arial">Active</font></b> </td>
+          <td> <b><font face="Arial">Scheduled</font></b> </td>
+      </tr>';
+        if ($result = $mysqli->query($query)) {
+            echo'<h3>Active Rentals</h3>';
+            while ($row = $result->fetch_assoc()) {
+                $RentalField1 = $row["RentalID"];
+                $RentalField2 = $row["IdNo"];
+                $RentalField3 = $row["VehicleID"];
+                $RentalField4 = $row["Term"];
+                $RentalField5 = $row["StartDate"];
+                $RentalField6 = $row["DailyTerm"];
+                $RentalField7 = $row["WeeklyTerm"];
+                $RentalField8 = $row["NoOfDays"];
+                $RentalField9 = $row["NoOfWeeks"];
+                $RentalField10 = $row["ActualReturnDate"];
+                $RentalField11 = $row["AmountDue"];
+                $RentalField12= $row["Active"];
+                $RentalField13 = $row["Scheduled"];
+
+                echo '<tr> 
+                  <td>'.$RentalField1.'</td> 
+                  <td>'.$RentalField2.'</td> 
+                  <td>'.$RentalField3.'</td> 
+                  <td>'.$RentalField4.'</td> 
+                  <td>'.$RentalField5.'</td> 
+                  <td>'.$RentalField6.'</td> 
+                  <td>'.$RentalField7.'</td> 
+                  <td>'.$RentalField8.'</td> 
+                  <td>'.$RentalField9.'</td> 
+                  <td>'.$RentalField10.'</td> 
+                  <td>'.$RentalField11.'</td> 
+                  <td>'.$RentalField12.'</td> 
+                  <td>'.$RentalField13.'</td> 
+              </tr>';
+            }
+            $result->free();
+        }
 
         ?>
     </div>
